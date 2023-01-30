88f5d9d19e32b9b8d9db5050e2d5c473ebe68f0a
Dominik Wajda <admin@epicsite.xyz>
2023-01-30 08:56:56 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+
+<ul>
+  <li><a href="/">SurveyPhunkey</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li style="float:right"><a class="active" href="#about">About</a></li>
+</ul>
+
+</body>
+</html>
+
+
+
+
+
+
 <?
 
