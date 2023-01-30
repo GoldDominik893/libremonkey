8647a25035bb679f43fd91da9b64fc5f3cba9d4b
Dominik Wajda <admin@epicsite.xyz>
2023-01-30 21:48:52 +0000

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,14 @@
 
 <ul>
   <li><a href="/">SurveyPhunkey</a></li>
-  <li><a href="#news">What we offer</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li style="float:right"><a class="active" href="#about">Login / Signup</a></li>
+  <li style="float:right"><a href="#about">Login / Signup</a></li>
 </ul>
-
+<div class="welcome"><h1>Create unlimited surveys for free.</h1><br>
+<a href="#" class="button">Make a Survey</a></div>
+<h1>
+Lorem ipsum dolor sit amet. Sit soluta explicabo eos corporis necessitatibus hic sapiente internos vel quia repellat. Non quos labore eos itaque sint cum alias quaerat et minus officiis ex voluptatem quibusdam aut delectus voluptatum. Et omnis saepe ea iste commodi aut pariatur omnis! Aut consequuntur laudantium ut culpa illum vel odit optio.<br><br>
+Qui architecto temporibus nam sunt illum et sequi impedit sit perferendis cupiditate aut quod tempore et laboriosam voluptatem sed obcaecati neque. Eum quae ratione ut perferendis quidem eos dolores delectus ut animi corrupti et veritatis libero 33 soluta fugiat. Est officiis galisum eos sequi maxime et magnam magnam ut eius natus ut consequatur accusamus aut culpa quia ut repellat voluptas.<br><br>
+In voluptatem facilis est maiores iste 33 voluptate nisi et omnis aperiam. Eum laborum numquam qui Quis atque ut deserunt nesciunt et aliquid sunt? Ut dignissimos facilis eum delectus obcaecati qui molestiae optio. Et consequatur illum ut laborum ducimus ut repellendus dolorem.<br><br>
+</h1>
 </body>
 </html>
