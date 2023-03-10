f898dfb1b28fce16df5f5055b08a3a97c11c135d
Dominik Wajda <admin@epicsite.xyz>
2023-03-10 20:33:34 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
-### LibreMonkey allows you to create unlimited online surveys with no ads at no cost.
-## Features
-- Create Surveys
-- No ads
-- No tracking
-- It's free
-
-
-## How it works
-#### LibreMonkey uses the PHP backend alongside MySQL to store surveys and survey responses. LibreMonkey is the clear winner when in comparison to all the others because it is fast and.
-
-## Screenshot of current stage
-![](/images/currentstage.png)
-##### disclaimer: its not done lol
+# surveyphunkey
--- index.php
+++ index.php
@@ -6,15 +6,64 @@
 <body>
 
 <ul>
-  <li><a href="/">SurveyPhunkey</a></li>
-  <li style="float:right"><a href="#about">Login / Signup</a></li>
+  <li><a href="/">LibreMonkey</a></li>
+  <li style="float:right"><a href="#">Login / Signup</a></li>
 </ul>
 <div class="welcome"><h1>Create unlimited surveys for free.</h1><br>
-<a href="#" class="button">Make a Survey</a></div>
-<h1>
-Lorem ipsum dolor sit amet. Sit soluta explicabo eos corporis necessitatibus hic sapiente internos vel quia repellat. Non quos labore eos itaque sint cum alias quaerat et minus officiis ex voluptatem quibusdam aut delectus voluptatum. Et omnis saepe ea iste commodi aut pariatur omnis! Aut consequuntur laudantium ut culpa illum vel odit optio.<br><br>
-Qui architecto temporibus nam sunt illum et sequi impedit sit perferendis cupiditate aut quod tempore et laboriosam voluptatem sed obcaecati neque. Eum quae ratione ut perferendis quidem eos dolores delectus ut animi corrupti et veritatis libero 33 soluta fugiat. Est officiis galisum eos sequi maxime et magnam magnam ut eius natus ut consequatur accusamus aut culpa quia ut repellat voluptas.<br><br>
-In voluptatem facilis est maiores iste 33 voluptate nisi et omnis aperiam. Eum laborum numquam qui Quis atque ut deserunt nesciunt et aliquid sunt? Ut dignissimos facilis eum delectus obcaecati qui molestiae optio. Et consequatur illum ut laborum ducimus ut repellendus dolorem.<br><br>
-</h1>
+<a href="#" class="button">Get Started</a></div>
+
+<div class="center"><h1>LibreMonkey lets you create unlimited surveys at no cost, we have all sorts of<br> 
+survey functions such as dropdowns, radio buttons, date and telephone numbers.<br>
+</h1></div>
+
+<div class="how-we-compare center">
+<br><br><br><h1>How we compare</h1>
+
+<table align="center">
+  <tr>
+    <th></th>
+    <th>LibreMonkey</th>
+    <th>SurveyMonkey(free)</th>
+    <th>Microsoft Forms(free)</th>
+    <th>Google Forms(free)</th>
+  </tr>
+  <tr>
+    <td><b>Question Limit</b></td>
+    <td>Unlimited</td>
+    <td>10</td>
+    <td>200</td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>Survey Limit</b></td>
+    <td>Unlimited</td>
+    <td></td>
+    <td>200</td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>Responses Limit</b></td>
+    <td>Unlimited</td>
+    <td>100</td>
+    <td>200</td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>Question Types</b></td>
+    <td>0</td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table><br><br>
+
+</div>
 </body>
 </html>
