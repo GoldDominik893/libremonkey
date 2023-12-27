81d89068726ba81111da4d89eecd39c2b8bb0f0c
Dominik Wajda <dominikwajda@hotmail.com>
2023-12-27 12:26:44 +0100

    add the latest changes

---
+++ form/not-logged-in.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="../styles/main.css">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+<body class="loginbg-color">
+<ul>
+  <li><a href="../">LibreMonkey</a></li>
+  <?php 
+  if ($_SESSION['logged_in'] == true) {
+   header('Location: ../');
+   exit(0);
+  } ?>
+</ul>
+
+
+<div class="loginbg">
+<h1 class="center">Client Area</h1>
+    <div class="center-container2 whitebg">
+        401 Unauthorised - You may not fill out this form because you are not logged in.
+    </div><br>
+</div>
+</body>
+</html>
